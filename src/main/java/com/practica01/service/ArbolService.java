b0c4942cfd861de8ae0c5807e9b2a07a86965419
MarcoDGCH19 <marcochaves0919@gmail.com>
2023-10-27 12:09:30 -0600

    Final

--- src/main/java/com/practica01/service/ArbolService.java
+++ src/main/java/com/practica01/service/ArbolService.java
@@ -9,12 +9,12 @@ public interface ArbolService {
  
     public List<Arbol> getArbols(boolean activo);
 
-    //Se obtiene una arbol segun el id pasado por parámetro
+    
     public Arbol getArbol(Arbol arbol);
 
-    //Se actualiza una arbol o se inserta un nueva.. (Si no hay id es un insert)
+   
     public void save(Arbol arbol);
 
-    //Se elimina una arbol segun el id pasado
+  
     public void delete(Arbol arbol);
 }
